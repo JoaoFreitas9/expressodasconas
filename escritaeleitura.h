4fc1ad163ac1ab998b9114eab15273af2f075fe2
rodri11sb <63006952+rodri11sb@users.noreply.github.com>
2020-05-19 16:35:01 +0100

    Create escritaeleitura.h

---
+++ escritaeleitura.h
@@ -0,0 +1,11 @@
+#ifndef escritaeleitura_H
+#define escritaeleitura_H
+#include<string>
+#include<iostream>
+using namespace std;
+
+
+string* leprimeironome(string caminho);
+string* leultimonome(string caminho);
+string* leparagens(string caminho);
+#endif
