a8216713fe5596f62cef0bbf0e30918ed27bb9ac
rodri11sb <63006952+rodri11sb@users.noreply.github.com>
2020-05-19 16:59:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,19 +6,14 @@ using namespace std;
 #include"escritaeleitura.h"
 #include"passageiro.h"
 
-int main() {
+int main(){
 	srand(time(NULL));
 	locale::global(locale(""));
 	string* primeiro_nomes = leprimeironome("G:/Users/Rodrigo Silva/Desktop//primeiro_nome.txt");
 	string* ultimo_nomes = leultimonome("G:/Users/Rodrigo Silva/Desktop//ultimo_nome.txt");
 	string* paragens_nomes = leparagens("G:/Users/Rodrigo Silva/Desktop//paragens.txt");
-	int* n_bilhetes = new int[10000];
-	int i = 0;
-	while (i < 10000) {
-		n_bilhetes[i] = i;
-		i = i + 1;
-	}
-	int n_paragens = rand() % 5 + 6;
+	int* n_bilhetes = gera_lista_bilhetes();
+	int n_paragens= rand() % 5 + 6;
 	char letras[26] = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
 	int numeros[10] = { 0,1,2,3,4,5,6,7,8,9 };
 	nodo_passageiro* lista_de_espera = new nodo_passageiro;
@@ -47,7 +42,7 @@ int main() {
 				cout << "5. Remover bilhete da paragem" << endl;
 				cout << "0. Voltar" << endl;
 				std::cin >> selecao;
-				switch (selecao) 
+				switch (selecao)
 				{
 				case '1':
 					cout << "***Escolheu a opção Remover passageiros nos autocarros***" << endl;
@@ -76,6 +71,7 @@ int main() {
 			break;
 		}
 	}
-std::cin.sync();
-std::cin.get();
+	std::cin.sync();
+	std::cin.get();
+	return 0;
 }
