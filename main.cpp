3f5bda92182859c06986f894bf1e8adab5c75f42
JoaoFreitas9 <61147222+JoaoFreitas9@users.noreply.github.com>
2020-05-19 16:51:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 using namespace std;
 #include <iostream>
 #include <locale>
@@ -7,7 +6,7 @@ using namespace std;
 #include"escritaeleitura.h"
 #include"passageiro.h"
 
-int main(){
+int main() {
 	srand(time(NULL));
 	locale::global(locale(""));
 	string* primeiro_nomes = leprimeironome("G:/Users/Rodrigo Silva/Desktop//primeiro_nome.txt");
@@ -19,9 +18,64 @@ int main(){
 		n_bilhetes[i] = i;
 		i = i + 1;
 	}
-	int n_paragens= rand() % 5 + 6;
+	int n_paragens = rand() % 5 + 6;
 	char letras[26] = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
 	int numeros[10] = { 0,1,2,3,4,5,6,7,8,9 };
 	nodo_passageiro* lista_de_espera = new nodo_passageiro;
 	lista_de_espera = gera_lista_de_espera(primeiro_nomes, ultimo_nomes, n_bilhetes);
+	char opcao;
+	bool sair = false;
+	while (!sair) {
+		bool voltar = false;
+		cout << "Escolha a sua opção:" << endl;
+		cout << "(s)eguinte ou (o)pções:  ";
+		std::cin >> opcao;
+		switch (opcao)
+		{
+		case 's':
+
+			break;
+		case 'o':
+			char selecao;
+			while (!voltar)
+			{
+				cout << "\nEscolha uma opcao:\n";
+				cout << "1. Remover passageiros nos autocarros" << endl;
+				cout << "2. Remover passageiros em fila de espera" << endl;
+				cout << "3. Apresentar bilhetes por paragem" << endl;
+				cout << "4. Alterar motorista" << endl;
+				cout << "5. Remover bilhete da paragem" << endl;
+				cout << "0. Voltar" << endl;
+				std::cin >> selecao;
+				switch (selecao) 
+				{
+				case '1':
+					cout << "***Escolheu a opção Remover passageiros nos autocarros***" << endl;
+					break;
+				case'2':
+					cout << "***Escolheu a opção Remover Passageiros em Fila de Espera***" << endl;
+					break;
+				case '3':
+					cout << "***Escolheu a opção Apresentar Bilhetes por Paragem***" << endl;
+					break;
+				case'4':
+					cout << "***Escolheu a opção Alterar Motorista***" << endl;
+					break;
+				case '5':
+					cout << "***Escolheu a opção Remover Bilhete da Paragem***" << endl;
+					break;
+				case'0':
+					cout << "***Escolheu a opção de voltar***" << endl;
+					voltar = true;
+					break;
+				}
+			}
+			break;
+		default:
+			cout << "Inseriu um caratere inválido. Prima ENTER para voltar " << endl;
+			break;
+		}
+	}
+std::cin.sync();
+std::cin.get();
 }
