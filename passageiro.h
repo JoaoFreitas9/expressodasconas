c0796fc20aaa1b935c2e633eb4ae5bdca7a925fe
rodri11sb <63006952+rodri11sb@users.noreply.github.com>
2020-05-19 16:32:35 +0100

    Create passageiro.h

---
+++ passageiro.h
@@ -0,0 +1,18 @@
+#ifndef passageiro_H
+#define passageiro_H
+#include<string>
+#include<iostream>
+using namespace std;
+struct passageiro {
+	string primeiro_nome;
+	string ultimo_nome;
+	int n_bilhete;
+};
+struct nodo_passageiro {
+	passageiro dados;
+	nodo_passageiro* seguinte;
+};
+nodo_passageiro* gera_lista_de_espera(string* primeiro_nomes, string* ultimo_nomes, int* n_bilhetes);
+nodo_passageiro* insereInicio(nodo_passageiro* inicio, passageiro valor);
+nodo_passageiro* criaNo(passageiro valor);
+#endif
