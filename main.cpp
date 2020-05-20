36f8fc1ee59ec86b8188a3a72f74ebdddc9bb91f
rodri11sb <63006952+rodri11sb@users.noreply.github.com>
2020-05-20 17:08:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-using namespace std;
-#include <iostream>
-#include <locale>
 #include <stdlib.h>
 #include <time.h>
 #include"escritaeleitura.h"
@@ -16,6 +13,7 @@ int main(){
 	int n_paragens= rand() % 5 + 6;
 	char matricula[36] = {'0','1','2','3','4','5','6','7','8','9', 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
 	nodo_passageiro* lista_de_espera = new nodo_passageiro;
+	int n_passageiros_lista_espera = 30;
 	lista_de_espera = gera_lista_de_espera(primeiro_nomes, ultimo_nomes, n_bilhetes);
 	char opcao;
 	bool sair = false;
