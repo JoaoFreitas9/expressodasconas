5a7af4beb82fc6d1b3a8eb91b596b15d7841be55
t-zinni <61590642+t-zinni@users.noreply.github.com>
2020-05-20 17:12:52 +0100

    Create alteraMotrorista.cpp

---
+++ alteraMotrorista.cpp
@@ -0,0 +1,12 @@
+void alteraMotorista(matricula* newMat) {
+string condutor
+cout << "Qual a matricula do autocarro?";
+cin >> newMat;
+while (a.seguinte = !NULL) {
+	if (newMat == matricula) {
+		cout << "Qual o nome do motorista" << ;
+		cin >> condutor;
+		}
+		
+	}
+}
