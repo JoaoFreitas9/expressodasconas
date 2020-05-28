438d13149f2f3c84dc796b933ec706a34fc94f74
rodri11sb <63006952+rodri11sb@users.noreply.github.com>
2020-05-28 18:12:48 +0100

    Update passageiro.h

--- passageiro.h
+++ passageiro.h
@@ -22,6 +22,7 @@ struct autocarro {
 	nodo_passageiro * utilizadores;
 };
 
+nodo_passageiro* adiciona_quinze(string* primeiro_nomes, string* ultimo_nomes, int* n_bilhetes, nodo_passageiro* lista_de_espera);
 void visualizacao(nodo_passageiro* espera);
 nodo_passageiro* gera_lista_de_espera(string* primeiro_nomes, string* ultimo_nomes, int* n_bilhetes);
 nodo_passageiro* insereInicio(nodo_passageiro* inicio, passageiro valor);
@@ -29,5 +30,5 @@ nodo_passageiro* criaNo(passageiro valor);
 nodo_passageiro* removefim(nodo_passageiro* head);
 int* gera_lista_bilhetes();
 string gera_matricula(char* caracteres);
-autocarro gera_autocarro(nodo_passageiro* lista_de_espera, int& n_passageiros_lista_espera, char* caracteres);
+autocarro gera_autocarro(string* primeiro_nome, string* ultimo_nome, nodo_passageiro* lista_de_espera, int& n_passageiros_lista_espera, char* caracteres);
 #endif
