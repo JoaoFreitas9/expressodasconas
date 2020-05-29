09fe25e9fb5c31c9f173d158426317d90f93d5fd
danygsilva9230 <63006987+danygsilva9230@users.noreply.github.com>
2020-05-29 10:05:53 +0100

    Update paragens.h

--- paragens.h
+++ paragens.h
@@ -1,16 +1,8 @@
 #ifndef PARAGENS_H
 #define PARAGENS_H
+#include <iostream>
+#include <string>
 using namespace std;
-
-struct paragem {
-	string nome_da_paragem;
-	node*arvore;
-};
-
-struct nodo_paragem {
-	paragem dados;
-	nodo_paragem* seguinte;
-};
 
 struct node {
 	int numero;
@@ -18,12 +10,18 @@ struct node {
 	node* direita;
 };
 
-nodo_paragem* criaNo_paragem(paragem valor);
-nodo_paragem* insereInicio_paragem(nodo_paragem* inicio, paragem valor);
-nodo_paragem* gera_paragens(string* paragens_nomes,int n_paragens);
+struct nodo_paragem {
+	nodo_paragem* seguinte;
+	node* raiz;
+	string nome_da_paragem;
+};
+
+nodo_paragem* insereInicio_paragem(nodo_paragem* inicio, nodo_paragem* novo);
+nodo_paragem* gera_paragens(string* paragens_nomes, int n_paragens);
 node* novo_no(int valor);
 node* inserir_no(node* raiz, int valor);
 void imprimeArvore(node* raiz, int nivel);
 node* pesquisar_no(node* raiz, int valor);
 node* remover_no(node* raiz, node* no, node* antes);
 #endif
+
