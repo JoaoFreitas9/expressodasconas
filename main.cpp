fc6fbe4ce359dd2848b10e5076fbedb762a3bd5a
rodri11sb <63006952+rodri11sb@users.noreply.github.com>
2020-06-01 17:24:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,28 +6,26 @@ using namespace std;
 #include"escritaeleitura.h"
 #include"passageiro.h"
 #include"paragens.h"
+#include"interacaopp.h"
 
-int main() {
+int main(){
 	srand(time(NULL));
 	locale::global(locale(""));
-	string* primeiro_nomes = leprimeironome("C:/Users/Dany/Desktop/PROJETO EDA EXPRESSO/primeiro_nome.txt");
-	string* ultimo_nomes = leultimonome("C:/Users/Dany/Desktop/PROJETO EDA EXPRESSO/ultimo_nome.txt");
-	string* paragens_nomes = leparagens("C:/Users/Dany/Desktop/PROJETO EDA EXPRESSO/paragens.txt");
+	string* primeiro_nomes = leprimeironome("primeiro_nome.txt");
+	string* ultimo_nomes = leultimonome("ultimo_nome.txt");
+	string* paragens_nomes = leparagens("paragens.txt");
 	int* n_bilhetes = gera_lista_bilhetes();
-	int n_paragens = rand() % 5 + 6;
-	char matricula[36] = { '0','1','2','3','4','5','6','7','8','9', 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
+	int n_paragens= rand() % 5 + 6;
+	char matricula[36] = {'0','1','2','3','4','5','6','7','8','9', 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
 	nodo_passageiro* lista_de_espera = NULL;
 	int n_passageiros_lista_espera = 30;
 	lista_de_espera = gera_lista_de_espera(primeiro_nomes, ultimo_nomes, n_bilhetes);
 	visualizacao(lista_de_espera);
-	//cout << primeiro_nomes[0] << endl;
 	nodo_paragem* lista_de_paragens = NULL;
 	lista_de_paragens = gera_paragens(paragens_nomes, n_paragens);
-	//nodo_paragem* temp = lista_de_paragens;
-	//while (temp != NULL) {
-		//cout << temp->dados.nome_da_paragem << endl;
-		//temp = temp->seguinte;
-	//}
+	nodo_autocarros* lista_de_autocarros = NULL;
+	autocarro primeiro_autocarro = gera_autocarro(primeiro_nomes, ultimo_nomes, lista_de_espera, n_passageiros_lista_espera, matricula);
+	lista_de_autocarros->dados = primeiro_autocarro;
 	char opcao;
 	bool sair = false;
 	while (!sair) {
@@ -38,7 +36,10 @@ int main() {
 		switch (opcao)
 		{
 		case 's':
-
+            primeiro_autocarro = gera_autocarro(primeiro_nomes, ultimo_nomes, lista_de_espera, n_passageiros_lista_espera, matricula);
+			lista_de_autocarros = iteracao_autocarro(lista_de_autocarros, primeiro_autocarro, n_paragens);
+			lista_de_espera=adiciona_quinze(primeiro_nomes,ultimo_nomes,n_bilhetes,lista_de_espera);
+			interacaoprograma(lista_de_autocarros,lista_de_paragens);
 			break;
 		case 'o':
 			char selecao;
