91c51dd8c28fb7ee00570238cc511c3a5d6019b0
rodri11sb <63006952+rodri11sb@users.noreply.github.com>
2020-06-01 17:01:42 +0100

    Create interacaopp.h

---
+++ interacaopp.h
@@ -0,0 +1,9 @@
+#ifndef INTERACAOPP_H
+#define INTERACAOPP_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+void interacaoprograma(nodo_autocarros* lista_de_autocarros, nodo_paragem* lista_de_paragens);
+
+#endif
