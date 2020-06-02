f19761b495e042a0c2d7728fd58ffb99c229be7a
JoaoFreitas9 <61147222+JoaoFreitas9@users.noreply.github.com>
2020-06-02 17:31:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,17 +6,17 @@ using namespace std;
 #include"escritaeleitura.h"
 #include"passageiro.h"
 #include"paragens.h"
-#include"interacaopp.h"
+#include "interacaopp.h"
 
-int main(){
+int main() {
 	srand(time(NULL));
 	locale::global(locale(""));
-	string* primeiro_nomes = leprimeironome("primeiro_nome.txt");
-	string* ultimo_nomes = leultimonome("ultimo_nome.txt");
-	string* paragens_nomes = leparagens("paragens.txt");
+	string* primeiro_nomes = leprimeironome("C:/Users/joaof/Desktop/primeiro_nome.txt");
+	string* ultimo_nomes = leultimonome("C:/Users/joaof/Desktop/ultimo_nome.txt");
+	string* paragens_nomes = leparagens("C:/Users/joaof/Desktop/paragens.txt");
 	int* n_bilhetes = gera_lista_bilhetes();
-	int n_paragens= rand() % 5 + 6;
-	char matricula[36] = {'0','1','2','3','4','5','6','7','8','9', 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
+	int n_paragens = rand() % 5 + 6;
+	char matricula[36] = { '0','1','2','3','4','5','6','7','8','9', 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
 	nodo_passageiro* lista_de_espera = NULL;
 	int n_passageiros_lista_espera = 30;
 	lista_de_espera = gera_lista_de_espera(primeiro_nomes, ultimo_nomes, n_bilhetes);
@@ -25,6 +25,11 @@ int main(){
 	nodo_autocarros* lista_de_autocarros = NULL;
 	autocarro primeiro_autocarro = gera_autocarro(primeiro_nomes, ultimo_nomes, lista_de_espera, n_passageiros_lista_espera, matricula);
 	lista_de_autocarros = insereInicio_autocarro(lista_de_autocarros, primeiro_autocarro);
+	nodo_passageiro* temp = primeiro_autocarro.utilizadores;
+	while (temp != NULL) {
+		cout << temp->dados.primeiro_nome<<endl;
+		temp = temp->seguinte;
+	}
 	visualizacao(lista_de_espera, lista_de_paragens, lista_de_autocarros );
 	char opcao;
 	bool sair = false;
@@ -36,10 +41,12 @@ int main(){
 		switch (opcao)
 		{
 		case 's':
-            primeiro_autocarro = gera_autocarro(primeiro_nomes, ultimo_nomes, lista_de_espera, n_passageiros_lista_espera, matricula);
+			system("CLS");
+			interacaoprograma(lista_de_autocarros, lista_de_paragens);
+			primeiro_autocarro = gera_autocarro(primeiro_nomes, ultimo_nomes, lista_de_espera, n_passageiros_lista_espera, matricula);
 			lista_de_autocarros = iteracao_autocarro(lista_de_autocarros, primeiro_autocarro, n_paragens);
-			lista_de_espera=adiciona_quinze(primeiro_nomes,ultimo_nomes,n_bilhetes,lista_de_espera);
-			interacaoprograma(lista_de_autocarros,lista_de_paragens);
+			lista_de_espera = adiciona_quinze(primeiro_nomes, ultimo_nomes, n_bilhetes, lista_de_espera);
+			visualizacao(lista_de_espera, lista_de_paragens, lista_de_autocarros);
 			break;
 		case 'o':
 			char selecao;
