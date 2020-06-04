09107a4ad0df69bd6e1dd9e446fa2cac79e46c6a
danygsilva9230 <63006987+danygsilva9230@users.noreply.github.com>
2020-06-04 17:59:05 +0100

    Update opcoes.h

--- opcoes.h
+++ opcoes.h
@@ -1,12 +1,13 @@
-#ifndef opcoes_H
-#define opcoes_H
 using namespace std;
 #include <iostream>
 #include <locale>
 #include <stdlib.h>
 #include <time.h>
 #include"passageiro.h"
+#include"paragens.h"
 
-void bilhete_crescente(nodo_autocarros* bus,nodo_paragem*paragem);
+void bilhete_crescente(nodo_autocarros* bus, nodo_paragem* paragem);
 void bilhete_por_arvore_binaria(nodo_autocarros* bus, nodo_paragem* paragens);
+nodo_autocarros* alterar_motorista(nodo_autocarros* bus);
+void remover_fila_espera(nodo_passageiro* lista_de_espera);
 #endif
