99cc9ae8ab23ed19fccb2f3a0ceb66f95c37d569
JoaoFreitas9 <61147222+JoaoFreitas9@users.noreply.github.com>
2020-06-04 16:15:30 +0100

    Update opcoes.h

--- opcoes.h
+++ opcoes.h
@@ -1,3 +1,5 @@
+#ifndef opcoes_H
+#define opcoes_H
 using namespace std;
 #include <iostream>
 #include <locale>
@@ -7,3 +9,4 @@ using namespace std;
 
 void bilhete_crescente(nodo_autocarros* bus,nodo_paragem*paragem);
 void bilhete_por_arvore_binaria(nodo_autocarros* bus, nodo_paragem* paragens);
+#endif
