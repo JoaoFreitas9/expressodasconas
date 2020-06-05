215d6b93d89898be3609a57d23ce5d0bcc4d1def
danygsilva9230 <63006987+danygsilva9230@users.noreply.github.com>
2020-06-05 18:54:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,9 +12,9 @@ using namespace std;
 int main() {
 	srand(time(NULL));
 	locale::global(locale(""));
-	string* primeiro_nomes = leprimeironome("C:/Users/joaof/Desktop/primeiro_nome.txt");
-	string* ultimo_nomes = leultimonome("C:/Users/joaof/Desktop/ultimo_nome.txt");
-	string* paragens_nomes = leparagens("C:/Users/joaof/Desktop/paragens.txt");
+	string* primeiro_nomes = leprimeironome("C:/Users/Dany/Desktop/PROJETO EDA EXPRESSO/primeiro_nome.txt");
+	string* ultimo_nomes = leultimonome("C:/Users/Dany/Desktop/PROJETO EDA EXPRESSO/ultimo_nome.txt");
+	string* paragens_nomes = leparagens("C:/Users/Dany/Desktop/PROJETO EDA EXPRESSO/paragens.txt");
 	int* n_bilhetes = gera_lista_bilhetes();
 	int n_paragens = rand() % 5 + 6;
 	char matricula[36] = { '0','1','2','3','4','5','6','7','8','9', 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
@@ -67,7 +67,7 @@ int main() {
 					break;
 				case'2':
 					cout << "***Escolheu a opção Remover Passageiros em Fila de Espera***" << endl;
-					remover_fila_espera(lista_de_espera);
+					lista_de_espera = remover_fila_espera(lista_de_espera);
 					visualizacao(lista_de_espera, lista_de_paragens, lista_de_autocarros);
 					break;
 				case '3':
